577cd6ff1ccb3caeec61c79d93fba042df79e8fd
Jacob Dixon <jacob.dixon@gmail.com>
2014-01-30 23:37:29 -0600

    add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,13 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def sign_out
     current_user.update_attribute(:remember_token,
                                   User.encrypt(User.new_remember_token))
@@ -31,7 +38,7 @@ module SessionsHelper
     self.current_user = nil
   end  
 
-  def redirect_back_or(default)
+ def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
     session.delete(:return_to)
   end
