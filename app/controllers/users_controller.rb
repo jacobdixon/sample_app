3921f19b58edcd234b825549890b1d872a7d6d55
Jacob Dixon <jacob.dixon@gmail.com>
2014-01-30 19:19:05 -0600

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to cs638!"
+      sign_in @user
+      flash[:success] = "Welcome to a cs638 twitter clone!"
       redirect_to @user
     else
       render 'new'
