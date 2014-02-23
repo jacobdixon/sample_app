04fd18b144ab73a32a6ccebdb74d788fdbf05f72
jacobdixon <jacob.dixon@gmail.com>
2014-02-23 13:33:50 -0600

    cleanup removal of paperclip code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,3 +26,4 @@ SampleApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,3 +77,4 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+end
