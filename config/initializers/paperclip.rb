67b5aa0a0a574a047fc7de2ce3754e70d12ecaa9
jacobdixon <jacob.dixon@gmail.com>
2014-02-23 13:36:19 -0600

    delete paperclip

--- config/initializers/paperclip.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-
-#If you are seeing the following error: “The bucket you are attempting to access must be addressed using the specified endpoint. Please send all future requests to this endpoint.” Try setting the specified endpoint with the s3_host_name config var.
-# config/initializers/paperclip.rb
-#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
